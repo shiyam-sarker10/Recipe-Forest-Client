8d60bb1d31be38298c5d366fae3f6d47633f4710
shiyam-sarker10 <kaneki438500929@gmail.com>
2023-12-13 21:13:28 +0600

    added responsive navbar

--- src/component/shared/Buttons/Button.jsx
+++ src/component/shared/Buttons/Button.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+
 
 const Button = ({text}) => {
     return (
-        <div className='bg-black px-6 py-4 text-white rounded'>
+        <div className='bg-black px-6 py-4 text-white rounded-lg'>
             {text}
         </div>
     );
--- src/component/shared/Nav/Nav.jsx
+++ src/component/shared/Nav/Nav.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import menu from '../../../assets/menu.svg'
+import cross from '../../../assets/cross.svg'
 import logo from '../../../assets/RecipeForest.png'
 import { NavLink } from 'react-router-dom';
 import Button from '../Buttons/Button';
+import { useState } from 'react';
 
 const Nav = () => {
+    const [isOpen,setIsOpen] = useState(false)
+
+
+
     const navList = (
       <>
-        <ul className='flex flex-col md:flex-row md:gap-10'>
+        <ul className='flex flex-col md:flex-row md:gap-10 '>
             {/* home  */}
           <li>
             <NavLink
@@ -55,17 +61,116 @@ const Nav = () => {
         </ul>
       </>
     );
+    const navListResponsive = (
+      <>
+        <ul className="flex flex-col gap-6 justify-center items-center ">
+          {/* home  */}
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "border-2 px-4 py-2 rounded-lg border-black"
+                  : ""
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+
+          {/* recipes  */}
+          <li>
+            <NavLink
+              to="/recipes"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "border-2 px-4 py-2 rounded-lg border-black"
+                  : ""
+              }
+            >
+              Recipes
+            </NavLink>
+          </li>
+          {/* Blog  */}
+          <li>
+            <NavLink
+              to="/blogs"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "border-2 px-4 py-2 rounded-lg border-black"
+                  : ""
+              }
+            >
+              Blogs
+            </NavLink>
+          </li>
+          {/* Contact  */}
+          <li>
+            <NavLink
+              to="/Contacts"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "border-2 px-4 py-2 rounded-lg border-black"
+                  : ""
+              }
+            >
+              Contact
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/Contacts"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "border-2 px-4 py-2 rounded-lg border-black"
+                  : ""
+              }
+            >
+              <Button text="Register"></Button>
+            </NavLink>
+          </li>
+        </ul>
+      </>
+    );
     return (
-      <div className='flex justify-between items-center px-4 py-6 shadow-md'>
+      <div className="flex md:justify-between md:items-center px-4 py-6 shadow-md">
         {/* image  */}
-        <div>
-          <img className='w-[175px]' src={logo} alt="" />
+        <div
+          className="
+        hidden md:block"
+        >
+          <img className="w-[175px] " src={logo} alt="" />
         </div>
+
+        
         {/* navlists */}
-        <div>{navList}</div>
+
+        <div className="hidden md:block">{navList}</div>
+        {/* menu button */}
+        <button className="flex items-start md:hidden" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? (
+            <img className="w-[25px] " src={cross} alt="" />
+          ) : (
+            <img className="w-[25px] " src={menu} alt="" />
+          )}
+        </button>
+        <div className={` ${isOpen ? "" : "hidden"}  md:hidden mx-auto`}>{navListResponsive}</div>
         {/* register  */}
-        <div>
-            <Button text="Register Now"></Button>
+        <div
+          className="
+        hidden md:block"
+        >
+          <Button text="Register Now"></Button>
         </div>
       </div>
     );
