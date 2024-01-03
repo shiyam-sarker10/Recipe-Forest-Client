8408c18a3d653e9d83a9333bfccbcd2d2731178d
shiyam-sarker10 <kaneki438500929@gmail.com>
2024-01-03 23:59:46 +0600

    responsive the banner

--- src/component/Home/Banner/BannerContent.jsx
+++ src/component/Home/Banner/BannerContent.jsx
@@ -3,50 +3,52 @@ import Fork from "../../../assets/ForkKnife.png";
 import timer from "../../../assets/Timer.png";
 import play from "../../../assets/PlayCircle.png";
 
-
 const BannerContent = ({ recipe }) => {
-    const {
-      foodTitle,
-      description,
-      timeToCook,
-      dishName,
-      image,
-      authorImage,
-      authorName,
-      dateOfPost
-    } = recipe;
+const {
+  foodTitle,
+  description,
+  timeToCook,
+  dishName,
+  image,
+  authorImage,
+  authorName,
+  dateOfPost,
+} = recipe;
 
   return (
-    <div className="flex h-[80vh] ">
-      <div className="bg-[#e7fafe] md:w-1/2 rounded-tl-3xl  rounded-bl-3xl p-8 lg:p-10 space-y-8 ">
+    <div className="flex flex-col-reverse md:flex-row h-[80vh]  ">
+      <div className="bg-[#e7fafe]  px-6 pb-6 md:p-6 lg:p-10 space-y-6 rounded-bl-3xl  rounded-br-3xl  md:w-1/2 md:rounded-tl-3xl  md:rounded-bl-3xl  md:rounded-br-none ">
+        {/* hot Recipe  */}
         <div className="bg-white py-2 hidden  px-4 rounded-full md:flex items-center gap-2 w-max">
           <img src={hotRecipe} alt="" />
           <span className="font-semibold text-sm">Hot Recipes</span>
         </div>
 
-        <div className="space-y-4">
+        <div className="space-y-4 md:space-y-6  ">
           {/* heading and desc  */}
-          <h1 className=" text-[26px] md:text-3xl lg:text-4xl xl:text-5xl font-semibold w-full lg:w-[90%]  lg:leading-[40px]">
+          <h1 className="  w-full text-[26px] md:text-3xl lg:text-4xl xl:text-5xl font-semibold  lg:w-[90%]  lg:leading-[40px]">
             {foodTitle}
           </h1>
           <p className="text-black/60 text-xs md:text-sm lg:text-base">
-            {description}
+            {description.slice(1)}
           </p>
 
-          {/* timer and formk  */}
+          {/* timer and form  */}
 
-          <div className="flex gap-4">
-            <div className="flex flex-col md:flex-row items-center gap-2 bg-black/5 py-2 px-2 lg:px-4 rounded-lg text-center lg:rounded-full">
-              <img className="" src={timer} alt="Time" />
-              <span className="text-xs text-black/60 font-semibold">
-                {timeToCook}
-              </span>
-            </div>
-            <div className="flex flex-col md:flex-row items-center gap-2 bg-black/5 py-2 px-2 lg:px-4 rounded-lg text-center lg:rounded-full">
-              <img className="" src={Fork} alt="Salad Rice" />
-              <span className="text-xs text-black/60 font-semibold">
-                {dishName}
-              </span>
+          <div className="hidden md:block">
+            <div className="flex gap-4 ">
+              <div className="flex flex-col md:flex-row items-center gap-2 bg-black/5 py-2 px-2 lg:px-4 rounded-lg text-center lg:rounded-full">
+                <img className="" src={timer} alt="Time" />
+                <span className="text-xs text-black/60 font-semibold">
+                  {timeToCook}
+                </span>
+              </div>
+              <div className="flex flex-col md:flex-row items-center gap-2 bg-black/5 py-2 px-2 lg:px-4 rounded-lg text-center lg:rounded-full">
+                <img className="" src={Fork} alt="Salad Rice" />
+                <span className="text-xs text-black/60 font-semibold">
+                  {dishName}
+                </span>
+              </div>
             </div>
           </div>
         </div>
@@ -63,11 +65,13 @@ const BannerContent = ({ recipe }) => {
               <p className="text-black/60 text-sm"> {dateOfPost}</p>
             </div>
           </div>
-          <button className="px-4 w-max text-xs md:text-sm md:text active:bg-black/10 active:border active:border-black active:text-black duration-500 flex gap-2 items-center rounded-lg py-3  bg-black text-white ">
-            View Recipe <img className="block md:hidden lg:block" src={play} alt="" />
+          <button className="px-4 md:px-2 lg:px-4 w-max text-xs md:text-sm md:text active:bg-black/10 active:border active:border-black active:text-black duration-500 flex gap-2 items-center rounded-lg py-3  bg-black text-white ">
+            View Recipe{" "}
+            <img className="block md:hidden lg:block" src={play} alt="" />
           </button>
         </div>
       </div>
+
       <div
         style={{
           backgroundImage: `url(${image})`,
@@ -75,7 +79,7 @@ const BannerContent = ({ recipe }) => {
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
-        className="bg-blue-500 hidden md:block w-1/2 rounded-tr-3xl  rounded-br-3xl"
+        className="bg-blue-500 w-full h-full  md:w-1/2 md:rounded-tr-3xl  md:rounded-br-3xl rounded-tr-3xl rounded-tl-3xl md:rounded-tl-none"
       ></div>
     </div>
   );
